aac09a2db8fdc7016f9e17c6b01e72362605991c
Takuto <tiharu717@gmail.com>
2023-08-29 10:20:09 +0900

    add Index

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+ 
+const IndexPage: NextPage = () => {
+  return <div>猫画像予定地</div>;
+};
+//NextPageはページコンポーネントを表すかた
+//これをすると、ページコンポーネントの硫黄権を満たすかチェックできる
+export default IndexPage;
+//export defaultをページコンポーネントとして認識
